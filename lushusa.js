9755fb7c07c3694c0b3c31f86d0fa7b9d59413ec
StanFaifor <119562928+StanFaifor@users.noreply.github.com>
2023-02-14 23:35:32 -0800

    The main header javascript file added and works

--- lushusa.js
+++ lushusa.js
@@ -2,3 +2,13 @@ let subMenu = document.getElementById("subMenu");
 function toggleMenu() {
   subMenu.classList.toggle("open-menu");
 }
+
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu");
+window.onscroll = function () {
+  if (window.pageYOffset >= menu.offsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
